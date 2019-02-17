1408f507e7bb472695a1730274179dcfd555ffae
Sveinbjörn <raudbjorn@gmail.com>
2019-02-17 19:39:42 +0000

    shutdown code, some architechting experiments

--- app/repositories/db/DbCall.java
+++ app/repositories/db/DbCall.java
@@ -1,12 +1,29 @@
 package repositories.db;
 
+import play.db.ConnectionCallable;
+import play.db.Database;
+
 import java.util.concurrent.CompletionStage;
 
+@FunctionalInterface
 public interface DbCall {
 
-    public static CompletionStage<Boolean> transactionally(DbCall... calls){
+    <T> CompletionStage<T> call(ConnectionCallable<T> callable);
+
+
+    public static CompletionStage<Boolean> transactionally(DbCall... calls) {
         return null;
     }
 
+
+    default DbCall withDatabase(Database db) {
+        return null;
+    }
+
+    default DbCall withExecutionContext(DatabaseExecutionContext context) {
+        return null;
+    }
+
+    //CompletableFuture.supplyAsync(() -> db.withConnection(true, block), context)
 
 }
