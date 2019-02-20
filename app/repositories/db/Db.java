1b61aef07a4ec09c15f6b53c6b03728dc6d5b5fa
sg <sg@korta.is>
2019-02-20 18:02:25 +0000

    done

--- app/repositories/db/Db.java
+++ app/repositories/db/Db.java
@@ -18,6 +18,23 @@ import static util.Throwables.propagate;
 @Singleton
 public class Db {
 
+    /*
+
+    CREATE TRIGGER UPDATE_STATUS_TIME AFTER UPDATE OF STATUS ON TASK
+  FOR EACH ROW
+BEGIN
+  UPDATE TASK SET SUBMITTED = strftime('%s', 'now') WHERE lower(status) = 'submitted' AND SUBMITTED IS NULL AND ID = NEW.ID;
+  UPDATE TASK SET STARTED = strftime('%s', 'now') WHERE lower(status) = 'started' AND STARTED IS NULL AND ID = NEW.ID;
+  UPDATE TASK SET COMPLETED = strftime('%s', 'now') WHERE lower(status) = 'completed' AND COMPLETED IS NULL AND ID = NEW.ID;
+END;
+
+SELECT * FROM PROBLEM;
+
+SELECT * FROM SOLUTION;
+
+SELECT * FROM TASK;
+     */
+
     private final Database db;
     private final DatabaseExecutionContext executionContext;
 
