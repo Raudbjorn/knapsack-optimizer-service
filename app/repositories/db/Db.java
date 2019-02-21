dc6f642d847a59ca11781ff98de40073ddf89674
sg <sg@korta.is>
2019-02-21 09:12:45 +0000

    removed unused db code

--- app/repositories/db/Db.java
+++ app/repositories/db/Db.java
@@ -6,34 +6,12 @@ import play.db.Database;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Function;
-import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toMap;
-import static util.Throwables.propagate;
 
 @Singleton
 public class Db {
-
-    /*
-
-    CREATE TRIGGER UPDATE_STATUS_TIME AFTER UPDATE OF STATUS ON TASK
-  FOR EACH ROW
-BEGIN
-  UPDATE TASK SET SUBMITTED = strftime('%s', 'now') WHERE lower(status) = 'submitted' AND SUBMITTED IS NULL AND ID = NEW.ID;
-  UPDATE TASK SET STARTED = strftime('%s', 'now') WHERE lower(status) = 'started' AND STARTED IS NULL AND ID = NEW.ID;
-  UPDATE TASK SET COMPLETED = strftime('%s', 'now') WHERE lower(status) = 'completed' AND COMPLETED IS NULL AND ID = NEW.ID;
-END;
-
-SELECT * FROM PROBLEM;
-
-SELECT * FROM SOLUTION;
-
-SELECT * FROM TASK;
-     */
 
     private final Database db;
     private final DatabaseExecutionContext executionContext;
@@ -44,28 +22,9 @@ SELECT * FROM TASK;
         this.executionContext = context;
     }
 
-    public CompletionStage<Map<Integer, ?>> transactionally(ConnectionCallable<?>... calls) {
-        return CompletableFuture.supplyAsync(() -> db.withConnection(connection -> {
-            connection.setAutoCommit(false);
-            Map<Integer, ?> result = IntStream
-                    .range(0, calls.length)
-                    .boxed()
-                    .collect(
-                            toMap(
-                                    propagate(i -> i),
-                                    propagate(i -> calls[i].call(connection))
-                            )
-                    );
-            connection.commit();
-            return result;
-        }), executionContext);
-    }
-
     public <T> CompletionStage<T> call(ConnectionCallable<T> call) {
         return CompletableFuture.supplyAsync(() -> db.withConnection(call), executionContext);
     }
-
-
 
     public <T,K> CompletionStage<K> chain(ConnectionCallable<T> block, Function<T, ConnectionCallable<K>> chainFunction){
         return CompletableFuture.supplyAsync(() -> db.withConnection(connection -> {
